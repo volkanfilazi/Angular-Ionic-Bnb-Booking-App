0d15db9e642541a0d5405f5dd3f227b6292253ca
Volkan Filazi <volkanfilazi@Volkans-MacBook-Air.local>
2023-08-09 20:35:24 +0200

    added swipeable bookings and delete function for booking page

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from './booking.service';
+import { Booking } from './booking.model';
+import { AlertController, IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
-
-  constructor() { }
+  allBookings?: Booking [];
+  constructor(
+    private bookingService: BookingService,
+    private alertCtrl: AlertController
+    ) { }
 
   ngOnInit() {
+    this.allBookings = this.bookingService.bookings;
   }
+
+  onCancelBooking(bookingId: string, slidingBooking: IonItemSliding) {
+    console.log(bookingId);
+    slidingBooking.close()
+    this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'Do you realy want to delete the book?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            if(bookingId){
+              this.bookingService.deleteBooking(bookingId);
+              this.allBookings = this.bookingService.bookings;
+            }
+          }
+        }
+      ]
+    }).then(alertEl => {
+      alertEl.present();
+    })
+  }
+
 
 }
