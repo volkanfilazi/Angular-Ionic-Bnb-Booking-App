fcc39d5cf66a75642ddb2fd565d7351cc3974ff6
Volkan Filazi <volkanfilazi@Volkans-MacBook-Air.local>
2023-08-15 18:53:45 +0200

    added register and login logics

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,24 +7,28 @@ import { AuthPage } from './auth.page';
   providedIn: 'root'
 })
 export class AuthService {
-  private _checkAuth = new BehaviorSubject<boolean>(false);
-  private _Users = new BehaviorSubject<User[]>([
-    {
-      userId: Math.random().toString(),
-      firstName: 'volkan',
-      lastName: 'filazi',
-      email: 'admin@hotmail.com',
-      password: 'admin91',
-      phonenumber: 12345678
-    }
-  ])
+  private _checkAuth = false;
+  private _loginErrorMessage = new BehaviorSubject<string>('');
+  private _localUsers = <User[]>[]
 
-  get Users() {
-    return this._Users.asObservable();
+  localUsersTrigger() {
+    const test = localStorage.getItem("userList")
+    if (test) {
+      const test2 = JSON.parse(test)
+      this._localUsers = test2
+    }
   }
 
+  get localUsers() {
+
+    return this._localUsers;
+  }
   get checkAuth() {
-    return this._checkAuth.asObservable();
+    return this._checkAuth;
+  }
+
+  get loginErrorMessage() {
+    return this._loginErrorMessage.asObservable();
   }
 
   private _userIsAuthenticated = false
@@ -40,26 +44,27 @@ export class AuthService {
   constructor() { }
 
   login(email: string, password: string) {
-    this.Users.pipe(
-      map(users => {
-        users.find(p => {
-          if(p.email === email && p.password === password) {
-            this._checkAuth.next(true);
-            this._userIsAuthenticated = true
-            console.log("service",this.checkAuth);
-          }else {
-            this._checkAuth.next(false) 
-          }
-        });
-      })
-    ).subscribe();
+    this.localUsersTrigger();
+    this.localUsers.find(p => {
+      console.log("test");
+      
+      if (p.email === email && p.password === password) {
+        this._checkAuth = true
+        console.log("hey",this.checkAuth);
+        
+        this._userIsAuthenticated = true
+      }else {
+        this._checkAuth = false
+      }
+    })
   }
 
   logout() {
     this._userIsAuthenticated = false
   }
 
-  register(firstName:string, lastName: string, email: string, password: string, phonenumber: number) {
+  register(firstName: string, lastName: string, email: string, password: string, phonenumber: number) {
+
     const newUser: User = {
       userId: Math.random().toString(),
       firstName: firstName,
@@ -68,8 +73,14 @@ export class AuthService {
       password: password,
       phonenumber: phonenumber
     };
-    this.Users.pipe(take(1), delay(1000), tap(p => {
-      this._Users.next(p.concat(newUser))
-    }));
+    this.localUsers.push(newUser)
+    // this.Users.pipe(take(1), delay(1000), tap(p => {
+    //   this._Users.next(p.concat(newUser))
+    // }));
+    let userlist = JSON.stringify(this.localUsers)
+    localStorage.setItem("userList", userlist)
+    this.localUsersTrigger()
+
+
   }
 }
